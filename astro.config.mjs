b60dcad596bb6770f99d4fe22a9c5cca007e80dc
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2023-06-19 13:58:02 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://iswwg88.coolify.bear.dedyn.io/",
   integrations: [
     tailwind(),
     image({
